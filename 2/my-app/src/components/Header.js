470b7ee81b4c059c817d124d93e8a8abdb8a686b
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-04-19 13:56:26 +0430

    header added

--- 2/my-app/src/components/Header.js
+++ 2/my-app/src/components/Header.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Divider from "@mui/material/Divider";
 
-const Header = () => {
+export default function ButtonAppBar() {
   return (
-    <div>Header</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              my skills within
+            </Typography>
+            <Typography variant="h2" component="div" sx={{ flexGrow: 1 }}>
+              Material UI
+            </Typography>
+          </Box>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Header
